4e39065cc44404f3cf96e50c7050e0109f9a206d
Raniya-thayyil <18gcs22@meaec.edu.in>
2022-03-03 15:24:40 +0530

    added new label

--- simple_window.py
+++ simple_window.py
@@ -2,7 +2,8 @@ import tkinter
 from tkinter.ttk import Button
 window=tkinter.Tk()
 window.title("edureka")
-label=tkinter.Label(window, text="nidha salam",font=("Arial",40)).pack()
+tkinter.Label(window, text="nidha salam",font=("Arial",40)).pack()
+tkinter.Label(window, text="raniya",font=("Arial",40)).pack()
 bt= Button (window,text="Enter")
 bt.pack(side=tkinter.TOP)
 window.mainloop()
