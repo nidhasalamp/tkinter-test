3d5856218480ddcbc56a4d306d0d44c93b6e4a4d
nidhasalamp <nidhasalamp@gmail.com>
2022-03-03 15:34:22 +0530

    new feature added

---
+++ win.py
@@ -0,0 +1,9 @@
+import tkinter
+from tkinter.ttk import Button
+window=tkinter.Tk()
+window.title("edureka")
+tkinter.Label(window, text="nidha salam",font=("Arial",40)).pack()
+tkinter.Label(window, text="raniya",font=("Arial",40)).pack()
+bt= Button (window,text="Enter")
+bt.pack(side=tkinter.TOP)
+window.mainloop()
